340ba87ad3de771697db7afa3e8f509b94f9a794
kathy-jiang <55174470+kathy-jiang@users.noreply.github.com>
2019-10-11 13:40:53 -0700

    Update diamond-lines.js

--- diamond-lines.js
+++ diamond-lines.js
@@ -1,21 +1,24 @@
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(255);
-  for (var x1=200; x1>=10; x1=x1-10){
-    for (var y1=10; y1<=200; y1=y1+10){
-      for (var a1=200: a1<=390; a1=a1+10){
-      line(x1,y1,a1,y1)
-    }
+  for (var i = 0; i <= 400; i = i + 10) {
+    line(0, i, 400, i);
   }
+  
+   {
+    
+    fill(259);
+    //noStroke();
+    triangle(0,0,200,0,0,200);
+    triangle(200,0,400,0,400,200);
+    triangle(0,200,0,400,200,400);
+    triangle(200,400,400,200,400,400);
+     
+             
 }
-  for(var x2=10; x2<=200; x2=x2+10){
-    for (var y2=200; y2<=400 ;y2=y2+10){
-      for(var a2= 390; a2>=200;a2=a2-10){
-        line(x2,y2,a2,y2)
-      }
-    }
-  }
+
 }
